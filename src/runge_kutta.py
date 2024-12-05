c2acef17765a00dde46712ef5a09e6bac7f49ca4
Nikita Korolev <niki.korolev@gmail.com>
2024-12-05 21:41:28 +0300

    Фикс шага и секущих

--- src/runge_kutta.py
+++ src/runge_kutta.py
@@ -67,11 +67,12 @@ def runge_kutta_4(u_0: list, t_0: float, t_k: float,
 
 
 def runge_kutta_4_condition(u_0: list, t_0: float, tau: float,
-                            function_right_side: Callable,
-                            eps_speed: float = 1e-4,
-                            eps_radius_vector: float = 1e-3,
-                            eps_angle: float = 1e-5,
-                            max_iterations: int = 15000):
+                            theta_1, theta_2, t_1, t_2, P_flag,
+                            eps_speed: float = 1e-12,
+                            eps_radius_vector: float = 1e-12,
+                            eps_angle: float = 1e-12,
+                            max_iterations: int = 15000,
+                            eps: float = 1e-7):
     """
     Решает систему ОДУ с помощью метода Рунге-Кутты 4-го порядка.
 
@@ -97,11 +98,108 @@ def runge_kutta_4_condition(u_0: list, t_0: float, tau: float,
     stop_condition_radius_vector = RADIUS_VECTOR_CONDITION(u[2], u[3])
     stop_condition_angle = ANGLE_CONDITION(u[0], u[1], u[2], u[3])
 
+    def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+        u, t, theta_1, theta_2, t_1, t_2, P)
+
     while abs(stop_condition_radius_vector) > eps_radius_vector or abs(stop_condition_angle) > eps_angle:
+        if P_flag:
+            def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 10.27 * 10 ** 3)
+        else:
+            def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 0)
+        if t < t_1 and t > T_V:
+            if t + tau > t_1 + eps:
+                if abs(t - t_1) < eps:
+                    t = t_1
+                    iterations += 1
+                    P_flag = False
+                    continue
+
+                t_p = t + tau
+                u = runge_kutta_4_step(u, t, t_1 - t, function_right_side)
+                t = t_1
+                solution.append(u[:])
+
+                iterations += 1
+                P_flag = False
+
+                if abs(t_1 - (t_p)) > eps:
+                    def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                        u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 0)
+
+                    u = runge_kutta_4_step(
+                        u, t, t_p - t_1, function_right_side)
+                    t = t_p
+                    solution.append(u[:])
+
+                    iterations += 1
+                continue
+
+            u = runge_kutta_4_step(u, t, tau, function_right_side)
+            solution.append(u[:])
+            t += tau
+            iterations += 1
+
+            continue
+
+        if t < T_V and t + tau > T_V + eps:
+            if abs(t - T_V) < eps:
+                t = T_V
+                iterations += 1
+                P_flag = True
+                continue
+            t_p = t + tau
+            u = runge_kutta_4_step(u, t, T_V - t, function_right_side)
+            t = T_V
+            solution.append(u[:])
+
+            P_flag = True
+            iterations += 1
+            if abs(T_V - (t_p)) > eps:
+                def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                    u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 10.27 * 10 ** 3)
+
+                u = runge_kutta_4_step(
+                    u, t, t_p - T_V, function_right_side)
+                t = t_p
+                solution.append(u[:])
+
+                iterations += 1
+            continue
+
+        if t < t_2 and t + tau > t_2:
+            if abs(t - t_2) < eps:
+                t = t_2
+                iterations += 1
+                P_flag = True
+                continue
+
+            t_p = t + tau
+            u = runge_kutta_4_step(u, t, t_2 - t, function_right_side)
+            t = t_2
+            solution.append(u[:])
+
+            P_flag = True
+            iterations += 1
+
+            if abs(t_2 - (t_p)) > eps:
+                def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                    u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 10.27 * 10 ** 3)
+                u = runge_kutta_4_step(
+                    u, t, t_p - t_2, function_right_side)
+                t = t_p
+                solution.append(u[:])
+
+                iterations += 1
+            continue
+
+        # Шаг метода Рунге-Кутты
         u = runge_kutta_4_step(u, t, tau, function_right_side)
 
         # Дробление шага
         if stop_condition_speed < eps_speed and SPEED_CONDITION(u[0], u[1]) > eps_speed:
+            # if stop_condition_angle < eps_angle and ANGLE_CONDITION(u[0], u[1], u[2], u[3]) > eps_angle:
             tau = tau / 2.
             u = solution[-1]
 
--- src/spacecraft_algorithm.py
+++ src/spacecraft_algorithm.py
@@ -9,7 +9,7 @@ from files import *
 
 def get_secant_method_step_for_theta(u: list, t: float, tau: float, theta_1: float,
                                      theta_2: float, t_1: float, t_2: float,
-                                     delta: float = 1e-12):
+                                     delta: float = 1e-8, P_flag=True):
     """
     Выполняет один шаг метода секущих для определения новых значений параметров theta_1 и theta_2
 
@@ -27,17 +27,14 @@ def get_secant_method_step_for_theta(u: list, t: float, tau: float, theta_1: flo
     - theta_1, theta_2 (float): Обновленные значения параметров theta_1 и theta_2.
     """
     # Считаем РК с приращениями по theta
-    def function_right_side_1(u, T): return FUNCTION_RIGHT_SIDE(
-        u, T, theta_1, theta_2, t_1, t_2)
-    u_1 = runge_kutta_4_condition(u, t, tau, function_right_side_1)[-1]
+    u_1 = runge_kutta_4_condition(
+        u, t, tau, theta_1, theta_2, t_1, t_2, P_flag)[-1]
 
-    def function_right_side_2(u, T): return FUNCTION_RIGHT_SIDE(
-        u, T, theta_1 + delta, theta_2, t_1, t_2)
-    u_2 = runge_kutta_4_condition(u, t, tau, function_right_side_2)[-1]
+    u_2 = runge_kutta_4_condition(
+        u, t, tau, theta_1 + delta, theta_2, t_1, t_2, P_flag)[-1]
 
-    def function_right_side_3(u, T): return FUNCTION_RIGHT_SIDE(
-        u, T, theta_1, theta_2 + delta, t_1, t_2)
-    u_3 = runge_kutta_4_condition(u, t, tau, function_right_side_3)[-1]
+    u_3 = runge_kutta_4_condition(
+        u, t, tau, theta_1, theta_2 + delta, t_1, t_2, P_flag)[-1]
 
     # Вычисляем значения функций условий для вычисления Якобиана
     dtheta_dt_1 = ANGLE_CONDITION(u_1[0], u_1[1], u_1[2], u_1[3])
@@ -52,8 +49,8 @@ def get_secant_method_step_for_theta(u: list, t: float, tau: float, theta_1: flo
          [(dr_dt_2 - dr_dt_1) / delta, (dr_dt_3 - dr_dt_1) / delta]]
 
     print(f"[INFO] Calculated jacobian: J[0, 0] = {
-        J[0][0]:.3f}, J[0, 1] = {J[0][1]:.3f}, J[1, 0] = {
-        J[1][0]:.3f}, J[1, 1] = {J[1][1]:.3f}.")
+        J[0][0]: .5f}, J[0, 1] = {J[0][1]: .5f}, J[1, 0] = {
+        J[1][0]: .5f}, J[1, 1] = {J[1][1]: .5f}.")
     print(f"[INFO] det|J| = {J[0][0] * J[1][1] - J[0][1] * J[1][0]}")
 
     # Шаг метода секущих
@@ -74,7 +71,7 @@ def get_secant_method_step_for_theta(u: list, t: float, tau: float, theta_1: flo
 
 def get_theta(u: list, t: float, tau: float, theta_1: float,
               theta_2: float, t_1: float, t_2: float, delta: float = 1e-5,
-              eps_radius_vector: float = 1e-7, eps_angle: float = 1e-9):
+              eps_radius_vector: float = 1e-7, eps_angle: float = 1e-9, P_flag=True):
     """
     Определяет параметры theta_1 и theta_2 с помощью метода секущих для обеспечения
     выполнения заданных условий по радиус-вектору и углу.
@@ -93,27 +90,30 @@ def get_theta(u: list, t: float, tau: float, theta_1: float,
     ----------
     - theta_1, theta_2 (float): Оптимальные значения параметров theta_1 и theta_2.
     """
-    print("[INFO] Start get_theta.")
+    # print("[INFO] Start get_theta.")
     stop_condition_radius_vector = RADIUS_VECTOR_CONDITION(u[2], u[3])
     stop_condition_angle = ANGLE_CONDITION(u[0], u[1], u[2], u[3])
+    iterations = 1
+    max_iterations = 20
 
     # Цикл метода секущих
     while abs(stop_condition_radius_vector) > eps_radius_vector or abs(stop_condition_angle) > eps_angle:
         # Делаем шаг метода секущих
         theta_1, theta_2 = get_secant_method_step_for_theta(
-            u, t, tau, theta_1, theta_2, t_1, t_2, delta)
+            u, t, tau, theta_1, theta_2, t_1, t_2, delta, P_flag)
 
-        # Считаем с новыми траекторию theta
-        def function_right_side(u, T): return FUNCTION_RIGHT_SIDE(
-            u, T, theta_1, theta_2, t_1, t_2)
-        u_new = runge_kutta_4_condition(u, t, tau, function_right_side)[-1]
+        u_new = runge_kutta_4_condition(
+            u, t, tau, theta_1, theta_2, t_1, t_2, P_flag)[-1]
 
         # Обновление значений критерия останова
         stop_condition_radius_vector = RADIUS_VECTOR_CONDITION(
             u_new[2], u_new[3])
         stop_condition_angle = ANGLE_CONDITION(
             u_new[0], u_new[1], u_new[2], u_new[3])
-    print("[INFO] Thetas finded.")
+        iterations += 1
+        if iterations > max_iterations:
+            break
+    # print("[INFO] Thetas finded.")
     return theta_1, theta_2
 
 
@@ -140,7 +140,7 @@ def spacecraft_algorithm(u_0: list, t_0: float, tau: float, theta_1: float, thet
     ----------
     - solution (list of lists): Список состояний космического аппарата на каждом временном шаге.
     """
-    print("[INFO] Run spacecraft_algorithm.")
+    # print("[INFO] Run spacecraft_algorithm.")
     initialize_output_file()
     solution = [u_0[:], ]
     u = u_0[:]
@@ -154,7 +154,6 @@ def spacecraft_algorithm(u_0: list, t_0: float, tau: float, theta_1: float, thet
         u, t, theta_1, theta_2, t_1, t_2, P)
 
     while abs(stop_condition) > eps:
-        log_iteration_values(t, u)
         if P_flag:
             def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
                 u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 10.27 * 10 ** 3)
@@ -172,31 +171,97 @@ def spacecraft_algorithm(u_0: list, t_0: float, tau: float, theta_1: float, thet
             ANGLE_CONDITION(u[0], u[1], u[2], u[3]):.7f}")
 
         # Отыскание theta_1 и theta_2 с помошью метода секущих
-        '''
         if ((t > T_V and t <= t_1) or t >= t_2) and not find_theta:
             theta_1, theta_2 = get_theta(
-                u, t, tau, theta_1, theta_2, t_1, t_2, delta)
+                u, t, tau, theta_1, theta_2, t_1, t_2, delta, P_flag)
             find_theta = True
 
-            def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
-                u, t, theta_1, theta_2, t_1, t_2)
-        '''
-
         if t < t_1 and t > T_V:
-            if abs(t - t_1) < eps:
+            if t + tau > t_1 + eps:
+                if abs(t - t_1) < eps:
+                    t = t_1
+                    iterations += 1
+                    P_flag = False
+                    continue
+
+                t_p = t + tau
+                u = runge_kutta_4_step(u, t, t_1 - t, function_right_side)
+                t = t_1
                 solution.append(u[:])
-                t += tau
+                log_iteration_values(t, u)
                 iterations += 1
                 P_flag = False
+
+                if abs(t_1 - (t_p)) > eps:
+                    def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                        u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 0)
+
+                    u = runge_kutta_4_step(
+                        u, t, t_p - t_1, function_right_side)
+                    t = t_p
+                    solution.append(u[:])
+                    log_iteration_values(t, u)
+                    iterations += 1
                 continue
 
-            if t + tau >= t_1:
-                u = runge_kutta_4_step(u, t, t_1 - t, function_right_side)
-                P_flag = False
             u = runge_kutta_4_step(u, t, tau, function_right_side)
             solution.append(u[:])
             t += tau
             iterations += 1
+            log_iteration_values(t, u)
+            continue
+
+        if t < T_V and t + tau > T_V + eps:
+            if abs(t - T_V) < eps:
+                t = T_V
+                iterations += 1
+                P_flag = True
+                continue
+            t_p = t + tau
+            u = runge_kutta_4_step(u, t, T_V - t, function_right_side)
+            t = T_V
+            solution.append(u[:])
+            log_iteration_values(t, u)
+            P_flag = True
+            iterations += 1
+            if abs(T_V - (t_p)) > eps:
+                def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                    u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 10.27 * 10 ** 3)
+
+                u = runge_kutta_4_step(
+                    u, t, t_p - T_V, function_right_side)
+                t = t_p
+                solution.append(u[:])
+                log_iteration_values(t, u)
+                iterations += 1
+            continue
+
+        if t < t_2 and t + tau > t_2:
+            if abs(t - t_2) < eps:
+                t = t_2
+                iterations += 1
+                P_flag = True
+                print(t, P_flag)
+                continue
+
+            t_p = t + tau
+            u = runge_kutta_4_step(u, t, t_2 - t, function_right_side)
+            t = t_2
+            solution.append(u[:])
+            log_iteration_values(t, u)
+            P_flag = True
+            iterations += 1
+
+            if abs(t_2 - (t_p)) > eps:
+                def function_right_side(u, t): return FUNCTION_RIGHT_SIDE(
+                    u, t, theta_1, theta_2, t_1, t_2, lambda t, t_1, t_2: 10.27 * 10 ** 3)
+                u = runge_kutta_4_step(
+                    u, t, t_p - t_2, function_right_side)
+                t = t_p
+                solution.append(u[:])
+                log_iteration_values(t, u)
+                iterations += 1
+            print(t, P_flag)
             continue
 
         # Шаг метода Рунге-Кутты
@@ -207,38 +272,20 @@ def spacecraft_algorithm(u_0: list, t_0: float, tau: float, theta_1: float, thet
             tau = tau / 2.
             t -= tau
             u = solution[-1]
-            print(f"[INFO] Crash step: tau: {tau}.")
+            # print(f"[INFO] Crash step: tau: {tau}.")
 
         stop_condition = SPEED_CONDITION(u[0], u[1])
-
-        if (t < T_V and t + tau > T_V):
-            P_flag = True
-            if abs(t - T_V) < eps:
-                solution.append(u[:])
-                t += tau
-                iterations += 1
-                continue
-            u = runge_kutta_4_step(u, t, T_V - t, function_right_side)
-
-        if (t < t_2 and t + tau > t_2):
-            P_flag = True
-            if abs(t - t_2) < eps:
-                solution.append(u[:])
-                t += tau
-                iterations += 1
-                continue
-            u = runge_kutta_4_step(u, t, t_2 - t, function_right_side)
-
         solution.append(u[:])
         t += tau
         iterations += 1
+        log_iteration_values(t, u)
 
         # Критерий останова по количеству итераций
         if iterations >= max_iterations:
-            print("[ERROR] Max iterations!")
+            # print("[ERROR] Max iterations!")
             break
 
-    print(f"[INFO] spacecraft_algorithm stopped!")
+    # print(f"[INFO] spacecraft_algorithm stopped!")
     return solution
 
 
@@ -267,4 +314,4 @@ def make_plot(solution: list, filename="img/spacecraft_algorithm.pdf"):
     plt.ylabel("y, м")
     plt.grid()
     plt.savefig(filename)
-    print(f"[INFO] Save plot at '{filename}'.")
+    # print(f"[INFO] Save plot at '{filename}'.")
